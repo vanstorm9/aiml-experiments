cafbe33be563e56fd149125c6f9e032a570fe4d4
Vanstorm9 <antlowhur@yahoo.com>
2016-05-31 15:02:56 -0400

    Added ability to load brain file

--- simple-chatbot/bot.py
+++ simple-chatbot/bot.py
@@ -1,20 +1,34 @@
 import aiml
 import os
 
+sessionId = 1
+
 # Create the kernel and learn AIML files
 kernel = aiml.Kernel()
 #kernel.learn("std-startup.xml")
 
+sessionData = kernel.getSessionData(sessionId)
+
 data_location = "./data/"
 
-files = [f for f in os.listdir(data_location) if f.endswith('.aiml')]
-for f in files:
-    kernel.learn(data_location + f)
-    print f
 
+brain_location = "./data/brain_files/bot_brain.brn"
 
-print(chr(27) + "[2J") # To clear terminal
+if os.path.isfile(brain_location):
+    kernel.bootstrap(brainFile = brain_location)
+else:
+	files = [f for f in os.listdir(data_location) if f.endswith('.aiml')]
+	for f in files:
+		kernel.learn(data_location + f)
+
+	kernel.saveBrain(brain_location)
+
 
 # Press CTRL-C to break this loop
 while True:
-    print kernel.respond(raw_input("Enter your message >> "))
+	message = raw_input("Enter your message >> ")
+	if(message == "exit()"):
+		print "See you later!"
+		exit()
+
+	print kernel.respond(message)
